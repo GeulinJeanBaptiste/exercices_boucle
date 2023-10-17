63553c150a2e35b136f5c996fcbdc8bc266ee509
jean-baptiste Geulin <jeanbaptiste.geulin@gmail.com>
2023-10-17 09:39:58 +0200

    exercices boucles

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+
+// IL n'y a pas forcement de tableau pour faire une boucle.
+// Boucle qui va de 3 à 23
+/* for (let i = 3; i < 23; i++) {
+  console.log(i);
+} */
+
+// Boucle qui va de 23 à 3 
+/* for (let i = 23; i > 2; i--) {
+    console.log(i);
+  } */
+  
+
+//   Boucle while qui va de 3 à 23 !!!! boucle  infini !!!!
+/* let bouclewhile = 3;
+while (bouclewhile < 23) {
+  bouclewhile++;
+  console.log(bouclewhile);
+} */
+
+// Boucle while qui va de 23 à 3 !!!! boucle infini !!!!
+/* let bouclewhile = 23;
+while (bouclewhile > 3) {
+  bouclewhile--;
+  console.log(bouclewhile);
+} */
+
+
+
+
+
+
+
